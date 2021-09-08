136788e2a7f8baa4a29c144cf100179a5ce10e36
PorfirioR <prezgonzlez.por@gmail.com>
2021-09-08 01:17:56 -0400

    #3

---
+++ OdonSysBackEnd/Sql/Entities/Client.cs
@@ -0,0 +1,15 @@
+﻿using Utilities.Enums;
+
+namespace Sql.Entities
+{
+    public class Client : BaseEntity
+    {
+        public string Name { get; set; }
+        public string SecondName { get; set; }
+        public string LastName { get; set; }
+        public string SecondLastName { get; set; }
+        public string Document { get; set; }
+        public string Ruc { get; set; }
+        public Country Country { get; set; }
+    }
+}
