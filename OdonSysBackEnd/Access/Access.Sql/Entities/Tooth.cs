da6f6a02d870dbc7e02b6c2d368dc806283b254f
PorfirioR <32472957+porfirioR@users.noreply.github.com>
2022-03-19 12:58:19 -0300

    #13

--- OdonSysBackEnd/Access/Access.Sql/Entities/Tooth.cs
+++ OdonSysBackEnd/Access/Access.Sql/Entities/Tooth.cs
@@ -8,6 +8,8 @@ namespace Sql.Entities
     {
         public int Number { get; set; }
         public string Name { get; set; }
+        public Jaw Jaw { get; set; }
+        public Quadrant Quadrant { get; set; }
         public DentalGroup Group { get; set; }
         virtual public IEnumerable<ProcedureTooth> ProcedureTeeth { get; set; }
     }
