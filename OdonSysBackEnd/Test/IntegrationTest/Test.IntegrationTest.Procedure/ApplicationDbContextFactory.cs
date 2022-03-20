2772e1994fd601bd60a1996c0c60f70b97687c51
PorfirioR <32472957+porfirioR@users.noreply.github.com>
2022-03-20 02:09:38 -0300

    #14

---
+++ OdonSysBackEnd/Test/IntegrationTest/Test.IntegrationTest.Procedure/ApplicationDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Access.Sql;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace DT.Host.Api.Acceptance.Test
+{
+    public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<DataContext>
+    {
+        public DataContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<DataContext>()
+                            .UseSqlServer("Server=(local);Database=DigitalTrustDocuments;Integrated Security=True;MultipleActiveResultSets=False");
+
+            return new DataContext(optionsBuilder.Options);
+        }
+    }
+}
