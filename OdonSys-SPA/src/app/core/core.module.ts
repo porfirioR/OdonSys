bcf43b6cea451670bd26d1b1e895f224350b1edd
Porfirio Perez <porfirioperezgon@gmail.com>
2022-03-29 12:18:15 -0400

    #19

--- OdonSys-SPA/src/app/core/core.module.ts
+++ OdonSys-SPA/src/app/core/core.module.ts
@@ -1,13 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { CoreRoutes } from './core.routing';
-import { AgGridModule } from 'ag-grid-angular';
 
 // Components
 import { HeaderComponent } from './components/header/header.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { PrincipalPageComponent } from './components/principal-page/principal-page.component';
+import { RouterModule } from '@angular/router';
+import { AgGridModule } from 'ag-grid-angular/public-api';
 
 @NgModule({
   imports: [
