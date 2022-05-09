5fff6bbfaee271e069b798d49ae1b5971f7d48e7
PorfirioR <prezgonzlez.por@gmail.com>
2022-05-09 00:36:03 -0400

    6#

--- OdonSys-SPA/src/app/admin/components/doctors/doctors.component.ts
+++ OdonSys-SPA/src/app/admin/components/doctors/doctors.component.ts
@@ -1,4 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ColDef, GridOptions } from 'ag-grid-community';
+import { throwError } from 'rxjs';
+import { catchError, first, tap } from 'rxjs/operators';
+import { ButtonGridActionType } from 'src/app/core/enums/button-grid-action-type.enum';
+import { GridActionModel } from 'src/app/core/models/view/grid-action-model';
+import { DoctorApiService } from 'src/app/core/services/api/doctor-api.service';
+import { AgGridService } from 'src/app/core/services/shared/ag-grid.service';
+import { AlertService } from 'src/app/core/services/shared/alert.service';
+import { DoctorModel } from '../../models/doctors/doctor-model';
 
 @Component({
   selector: 'app-doctors',
@@ -6,10 +16,88 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./doctors.component.scss']
 })
 export class DoctorsComponent implements OnInit {
+  public load: boolean = false;
+  public gridOptions!: GridOptions;
+  public procedureList: DoctorModel[] = [];
 
-  constructor() { }
+  constructor(
+    private readonly router: Router,
+    private readonly alertService: AlertService,
+    private readonly doctorApiService: DoctorApiService,
+    private readonly agGridService: AgGridService
+
+  ) { }
 
   ngOnInit() {
+    this.setupAgGrid();
+    this.load = true;
+    this.getList();
   }
 
+  private setupAgGrid = (): void => {
+    this.gridOptions = this.agGridService.getProcedureGridOptions();
+    const columnAction = this.gridOptions.columnDefs?.find((x: ColDef) => x.field === 'action') as ColDef;
+    const params: GridActionModel = {
+      buttonShow: [ButtonGridActionType.Borrar, ButtonGridActionType.Editar],
+      clicked: this.actionColumnClicked
+    };
+    columnAction.cellRendererParams = params;
+  }
+
+
+  private getList = () => {
+    this.doctorApiService.getAll().pipe(
+      tap(response => {
+        this.procedureList = Object.assign(Array<DoctorModel>(), response);
+        this.gridOptions.api?.setRowData(this.procedureList);
+        this.gridOptions.api?.sizeColumnsToFit();
+        if (this.procedureList.length === 0) {
+          this.gridOptions.api?.showNoRowsOverlay();
+        }
+      }), catchError(err => {
+        this.gridOptions.api?.showNoRowsOverlay();
+        return throwError(err);
+      })
+    ).subscribe();
+  }
+
+  private actionColumnClicked = (action: ButtonGridActionType): void => {
+    const currentRowNode = this.agGridService.getCurrentRowNode(this.gridOptions);
+    switch (action) {
+      case ButtonGridActionType.Ver:
+        this.router.navigate([`${this.router.url}/ver/${currentRowNode.data.id}`]);
+        break;
+      case ButtonGridActionType.Borrar:
+        this.deleteSelectedItem(currentRowNode.data.id);
+        break;
+      default:
+        break;
+    }
+  }
+
+  private deleteSelectedItem = (id: string): void => {
+    this.alertService.showQuestionModal('Estas seguro de eliminar el procedimiento?', 'Los cambios son permanentes.')
+    .then((result) => {
+      if (result.value) {
+        this.doctorApiService.delete(id).pipe(
+          first(),
+          tap(() => {
+            this.alertService.showSuccess('El procedimiento ha sido eliminado');
+            this.getList();
+          })
+        ).subscribe();
+      }
+    });
+  }
+
+  public approve = () => {
+    const currentRowNode = this.agGridService.getCurrentRowNode(this.gridOptions);
+    this.doctorApiService.approve(currentRowNode.data.id).pipe(
+      first(),
+      tap(() => {
+        this.alertService.showSuccess('El doctor ha sido habilitado para ingresar al sistema.');
+        this.getList();
+      })
+    ).subscribe();
+  }
 }
--- OdonSys-SPA/src/app/core/services/api/doctor-api.service.ts
+++ OdonSys-SPA/src/app/core/services/api/doctor-api.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from '../../../../environments/environment';
+import { DoctorApiModel } from '../../models/api/doctor/doctor-api-model';
 import { UpdateUserApiModel } from '../../models/users/update-user-api-model';
 import { UpdateUserRequest } from '../../models/users/update-user-request';
 
@@ -19,6 +20,19 @@ export class DoctorApiService {
 
   public updateConfiguration = (id: string, model: UpdateUserRequest): Observable<UpdateUserApiModel> => {
     return this.http.put<UpdateUserApiModel>(`${this.baseUrl}`, model);
+  }
+
+  
+  public getAll = (reference: boolean = false): Observable<DoctorApiModel[]> => {
+    return this.http.get<DoctorApiModel[]>(`${this.baseUrl}/${reference}`);
+  }
+
+  public delete = (id: string): Observable<object> => {
+    return this.http.delete(`${this.baseUrl}/${id}`);
+  }
+
+  public approve = (id: string): Observable<DoctorApiModel> => {
+    return this.http.put<DoctorApiModel>(`${this.baseUrl}/${id}`, null);
   }
 
 }
