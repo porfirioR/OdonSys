f34a2dabd957cb3743f37696ba58666f6f2eac24
porfirioR <porfirioperezgon@gmail.com>
2023-05-08 13:58:22 -0400

    Delete old migrations

--- OdonSysBackEnd/Access/Access.Sql/Migrations/DataContextModelSnapshot.cs
+++ OdonSysBackEnd/Access/Access.Sql/Migrations/DataContextModelSnapshot.cs
@@ -8,7 +8,7 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
 
-namespace Sql.Migrations
+namespace Access.Sql.Migrations
 {
     [DbContext(typeof(DataContext))]
     partial class DataContextModelSnapshot : ModelSnapshot
