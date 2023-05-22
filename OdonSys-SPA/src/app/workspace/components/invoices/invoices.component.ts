ac0f1428989414f7ac2f42f90026280069390d66
porfirioR <porfirioperezgon@gmail.com>
2023-05-22 14:31:33 -0400

    Invoice payments buttons

--- OdonSys-SPA/src/app/workspace/components/invoices/invoices.component.ts
+++ OdonSys-SPA/src/app/workspace/components/invoices/invoices.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ColDef, GridOptions } from 'ag-grid-community';
 import { GridActionModel } from '../../../core/models/view/grid-action-model';
 import { InvoiceApiModel } from '../../models/invoices/api/invoice-api-model';
+import { CustomGridButtonShow } from '../../../core/models/view/custom-grid-button-show';
 import { ButtonGridActionType } from '../../../core/enums/button-grid-action-type.enum';
+import { Permission } from '../../../core/enums/permission.enum';
 import { AgGridService } from '../../../core/services/shared/ag-grid.service';
 import { InvoiceApiService } from '../../services/invoice-api.service';
+import { UserInfoService } from '../../../core/services/shared/user-info.service';
+import { PaymentModalComponent } from '../../modals/payment-modal/payment-modal.component';
 
 @Component({
   selector: 'app-invoices',
@@ -16,14 +21,18 @@ export class InvoicesComponent implements OnInit {
   protected gridOptions!: GridOptions
   protected ready: boolean = false
   protected loading: boolean = false
+  private canRegisterPayments = false
 
   constructor(
     private readonly agGridService: AgGridService,
     private readonly router: Router,
-    private readonly invoiceApiService: InvoiceApiService
+    private readonly invoiceApiService: InvoiceApiService,
+    private readonly userInfoService: UserInfoService,
+    private modalService: NgbModal,
   ) { }
 
   ngOnInit() {
+    this.canRegisterPayments = this.userInfoService.havePermission(Permission.RegisterPayments)
     this.setupAgGrid()
     this.ready = true
     this.getList()
@@ -52,7 +61,8 @@ export class InvoicesComponent implements OnInit {
     const columnAction = this.gridOptions.columnDefs?.find((x: ColDef) => x.field === 'action') as ColDef
     const params: GridActionModel = {
       buttonShow: [ButtonGridActionType.Editar, ButtonGridActionType.Ver],
-      clicked: this.actionColumnClicked
+      clicked: this.actionColumnClicked,
+      customButton:  this.canRegisterPayments ? new CustomGridButtonShow(' Pagos', 'fa-money-bill') : undefined
     }
     columnAction.cellRendererParams = params
   }
@@ -65,6 +75,17 @@ export class InvoicesComponent implements OnInit {
         break
       case ButtonGridActionType.Editar:
         this.router.navigate([`${this.router.url}/actualizar/${currentRowNode.data.id}`])
+        break
+      case ButtonGridActionType.CustomButton:
+        const modalRef = this.modalService.open(PaymentModalComponent)
+        modalRef.componentInstance.userId = currentRowNode.data.id
+        modalRef.componentInstance.name = currentRowNode.data.name
+        modalRef.result.then((result) => {
+          if(result) {
+            currentRowNode.data.roles = result
+            // this.gridOptions.api?.refreshCells({ force: true, columns: ['roles'] })
+          }
+        }, () => {})
         break
       default:
         break
--- OdonSys-SPA/src/app/workspace/modals/payment-modal/payment-modal.component.ts
+++ OdonSys-SPA/src/app/workspace/modals/payment-modal/payment-modal.component.ts
@@ -7,15 +7,14 @@ import { CheckFormGroup } from '../../../core/forms/check-form-group';
 import { RoleApiService } from '../../../core/services/api/role-api.service';
 import { UserInfoService } from '../../../core/services/shared/user-info.service';
 import { AlertService } from '../../../core/services/shared/alert.service';
-import { UserApiService } from '../../services/user-api.service';
 import { DoctorApiService } from '../../../core/services/api/doctor-api.service';
 
 @Component({
-  selector: 'app-user-role',
-  templateUrl: './user-role.component.html',
-  styleUrls: ['./user-role.component.scss']
+  selector: 'app-payment-modal',
+  templateUrl: './payment-modal.component.html',
+  styleUrls: ['./payment-modal.component.scss']
 })
-export class UserRoleComponent implements OnInit {
+export class PaymentModalComponent implements OnInit {
   @Input() userId!: string
   @Input() name!: string
 
@@ -29,47 +28,46 @@ export class UserRoleComponent implements OnInit {
     private readonly rolesApiService: RoleApiService,
     private userInfoService: UserInfoService,
     private alertService: AlertService,
-    private readonly userApiService: UserApiService,
     private readonly doctorApiService: DoctorApiService,
   ) { }
 
   ngOnInit() {
-    combineLatest([this.rolesApiService.getAll(), this.doctorApiService.getById(this.userId)]).pipe(take(1)).subscribe({
-      next: ([roles, user]) => {
-        const userRoles = user.roles
-        roles.forEach(x => {
-          const formGroup = new FormGroup<CheckFormGroup>({
-            name: new FormControl(x.name),
-            code: new FormControl(x.code),
-            value: new FormControl(userRoles.includes(x.code))
-          })
-          this.formGroup.controls.roles.push(formGroup)
-        })
-        this.formGroup.controls.roles.addValidators(this.minimumOneSelectedValidator)
-      },
-      error: (e) => {
-        throw e;
-      }
-    })
+    // combineLatest([this.rolesApiService.getAll(), this.doctorApiService.getById(this.userId)]).pipe(take(1)).subscribe({
+    //   next: ([roles, user]) => {
+    //     const userRoles = user.roles
+    //     roles.forEach(x => {
+    //       const formGroup = new FormGroup<CheckFormGroup>({
+    //         name: new FormControl(x.name),
+    //         code: new FormControl(x.code),
+    //         value: new FormControl(userRoles.includes(x.code))
+    //       })
+    //       this.formGroup.controls.roles.push(formGroup)
+    //     })
+    //     this.formGroup.controls.roles.addValidators(this.minimumOneSelectedValidator)
+    //   },
+    //   error: (e) => {
+    //     throw e;
+    //   }
+    // })
   }
 
   protected save = () => {
     this.saving = true
     const roles = this.formGroup.value.roles!.filter(x => x.value).map(x => x.code!)
     const userRoles = new UserRoleApiRequest(this.userId, roles)
-    this.userApiService.setUserRoles(userRoles).pipe(take(1)).subscribe({
-      next: (response) => {
-        if(this.userInfoService.getUserData().id === this.userId) {
-          this.userInfoService.setRoles(response)
-        }
-        this.alertService.showSuccess('Lista de Roles actualizada correctamente.')
-        this.saving = false
-        this.activeModal.close(response)
-      }, error: (e) => {
-        this.saving = false
-        throw e
-      }
-    })
+    // this.userApiService.setUserRoles(userRoles).pipe(take(1)).subscribe({
+    //   next: (response) => {
+    //     if(this.userInfoService.getUserData().id === this.userId) {
+    //       this.userInfoService.setRoles(response)
+    //     }
+    //     this.alertService.showSuccess('Lista de Roles actualizada correctamente.')
+    //     this.saving = false
+    //     this.activeModal.close(response)
+    //   }, error: (e) => {
+    //     this.saving = false
+    //     throw e
+    //   }
+    // })
   }
 
   private minimumOneSelectedValidator = (abstractControl: AbstractControl): ValidationErrors | null => {
