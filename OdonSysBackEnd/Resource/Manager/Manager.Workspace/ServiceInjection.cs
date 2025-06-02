eeb14d4f5b6a931c806623cbca6203bcc543c765
PorfirioR <porfirioperezgon@gmail.com>
2025-06-01 23:59:39 -0300

    291-orthodontics-flow

--- OdonSysBackEnd/Resource/Manager/Manager.Workspace/ServiceInjection.cs
+++ OdonSysBackEnd/Resource/Manager/Manager.Workspace/ServiceInjection.cs
@@ -1,7 +1,9 @@
 ﻿using Contract.Workspace.Files;
+using Contract.Workspace.Orthodontics;
 using Contract.Workspace.Procedures;
 using Contract.Workspace.Teeth;
 using Manager.Workspace.Files;
+using Manager.Workspace.Orthodontics;
 using Manager.Workspace.Procedures;
 using Manager.Workspace.Teeth;
 using Microsoft.Extensions.DependencyInjection;
@@ -14,9 +16,11 @@ public class ServiceInjection
     {
         services.AddTransient<IProcedureManagerBuilder, ProcedureManagerBuilder>();
         services.AddTransient<IToothManagerBuilder, ToothManagerBuilder>();
+        services.AddTransient<IOrthodonticManagerBuilder, OrthodonticManagerBuilder>();
 
         services.AddTransient<IFileManager, FileManager>();
         services.AddTransient<IProcedureManager, ProcedureManager>();
         services.AddTransient<IToothManager, ToothManager>();
+        services.AddTransient<IOrthodonticManager, OrthodonticManager>();
     }
 }
